db48c68d32b27885642a338a531d46a1d8e14048
WhosShreky <158489264+WhosShreky@users.noreply.github.com>
2025-09-04 19:36:49 -0500

    creacion plantilla

---
+++ Caba/src/main/java/com/pagina/Caba/CabaApplication.java
@@ -0,0 +1,13 @@
+package com.pagina.Caba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabaApplication.class, args);
+	}
+
+}
---
+++ Caba/src/test/java/com/pagina/Caba/CabaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pagina.Caba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
